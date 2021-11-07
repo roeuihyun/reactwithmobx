c965efe5df54ebe7714d9e8d232b6685aed9b33d
roeuihyun <roeuihyun@gmail.com>
2021-11-08 08:18:55 +0900

    react with mobx basic education (4-3)

--- source-template/2.state-templete/src/App.js
+++ source-template/2.state-templete/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import {Container} from "@material-ui/core";
+import {Container, Grid} from "@material-ui/core";
 import SearchBar from "./components/SearchBar";
+import Books from "./static_data/Books";
+import BookList from "./components/BookList";
+import BookListItem from "./components/BookListItem";
 
 class App extends Component {
   render(){
     return(
       <Container>
         <SearchBar/>
+        <Grid container spacing={2}>
+            <Grid item>
+                <BookList book={Books}></BookList>
+            </Grid>
+        </Grid>
       </Container>
     )
   }
--- source-template/2.state-templete/src/components/BookList.js
+++ source-template/2.state-templete/src/components/BookList.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+import {List} from '@material-ui/core';
+import BookListItem from './BookListItem';
+
+
+class BookList extends Component{
+    render() {
+        const bookItems = this.props.book.map( book => {
+            return(
+                <BookListItem book={book} key={book.ISBN}>
+                </BookListItem>
+            )
+        })
+        return(
+            <List>
+                {bookItems}
+            </List>
+        )
+    }
+}
+
+export default BookList;
