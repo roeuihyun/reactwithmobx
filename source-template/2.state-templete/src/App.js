1918c5ab65dcb0acc90e81cfef1c1dc684e15ae3
roeuihyun <roeuihyun@gmail.com>
2021-11-13 12:26:27 +0900

    react with mobx basic education (4-4)

--- source-template/2.state-templete/src/App.js
+++ source-template/2.state-templete/src/App.js
@@ -3,16 +3,29 @@ import {Container, Grid} from "@material-ui/core";
 import SearchBar from "./components/SearchBar";
 import Books from "./static_data/Books";
 import BookList from "./components/BookList";
+import BookDetail from "./components/BookDetail";
 import BookListItem from "./components/BookListItem";
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          books : Books,
+          selectedBook : Books[0],
+      }
+  }
+
   render(){
     return(
       <Container>
         <SearchBar/>
         <Grid container spacing={2}>
             <Grid item>
-                <BookList book={Books}></BookList>
+                <BookList book={this.state.books}></BookList>
+            </Grid>
+            <Grid item>
+                <BookDetail book={this.state.selectedBook}></BookDetail>
             </Grid>
         </Grid>
       </Container>
